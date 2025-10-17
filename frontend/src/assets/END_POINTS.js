89986f9b810708476d8b84b964fde94ff6f2244e
Aman Kumar <aman.kumar@geeksforgeeks.org>
2025-10-17 17:45:32 +0530

    re:prod

--- frontend/src/assets/END_POINTS.js
+++ frontend/src/assets/END_POINTS.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://job-portal-8rp9.onrender.com";
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "https://job-portal-8rp9.onrender.com";
+// const BASE_URL = "http://localhost:3000"
 
 const END_POINTS = {
   //authentications
