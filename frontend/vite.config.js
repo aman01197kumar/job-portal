e4ef5c1e59eac2c738dfe62c040e414388aa1c48
Anand8198 <h9.amankumar@outlook.com>
2025-11-05 11:02:30 +0530

    modify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   optimizeDeps: {
-    include: ['@mui/material', '@mui/icons-material'],
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ["@mui/material", "@mui/icons-material"],
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1500, // Set the limit to 1500 KiB (1.5 MB)
+  },
+});
